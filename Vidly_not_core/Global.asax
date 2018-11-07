074421aee22c22151f6a5296522137740e19751f
Ferenc Király <kiraly.fecco@gmail.com>
2018-11-07 13:26:00 +0100

    initial commet at the start of the lecture

---
+++ Vidly_not_core/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Vidly_not_core.MvcApplication" Language="C#" %>
---
+++ Vidly_not_core/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidly_not_core.Startup))]
+namespace Vidly_not_core
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
